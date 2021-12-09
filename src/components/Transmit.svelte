2f7469bba9697d78a0925eda15891956b6d02b2d
Mamun <mmamun.roshid@gmail.com>
2021-12-09 19:17:18 +0600

    deploy20211209

--- src/components/Transmit.svelte
+++ src/components/Transmit.svelte
@@ -14,15 +14,15 @@
     Background(image="transmit_footer")
       .grid.three
         .grid_container.moved_right(style="--delay: 0.5")
-          a(href="https://wetransfer.com/downloads/bd8b332a7290e4dca9e259bb9a8659d320211130112108/1bc414e017d8da245174d2d4d914c52c20211130112136/b0b8bf" target="_blank")
+          a()
             img(src="/images/optimized/transmit_1.jpg")
           h3 print
         .grid_container.moved_right(style="--delay: 0.75")
-          a(href="http://www.bangladeshdenimtimes.com" target="_blank")
+          a()
             img(src="/images/optimized/transmit_2.jpg")
           h3 website
         .grid_container.moved_right(style="--delay: 1")
-          a(href="https://www.facebook.com/bangladeshdenimtimes" target="_blank")
+          a()
             img(src="/images/optimized/transmit_3.jpg")
           h3 facebook
 
